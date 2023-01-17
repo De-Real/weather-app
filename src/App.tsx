0a5f31a7e51cd0f890c51571210e8d5de04a2b12
Real <vinarchuk.a.v@.gmail.com>
2023-01-17 16:29:37 +0200

    updated 2

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,11 @@ import React, { useEffect } from 'react';
 import RoutesNavigation from './RoutesNavigation';
 const App: React.FC = () => {
 
-	// useEffect(() => {
-	// 	const fetchData = async () => {
+	useEffect(() => {
+		document.title = 'Weather App'
+	}, [])
 
-	// 		const response = await fetch('http://api.weatherapi.com/v1/forecast.json?key=2fd38a87e8c84cf493c171558230501&q=Bila Tserkva&days=14');
-	// 		if (!response.ok) {
-	// 			console.log(response.statusText);
-	// 		}
 
-	// 		interface FOEf { results: { current: {}, forecast: { forecastday: [] } } }
-
-	// 		const results = await response.json();
-
-	// 		console.log(results.forecast.forecastday[0].hour[0]);
-
-	// 	}
-	// 	fetchData();
-	// }, [])
-
-	
 	return (
 		<main>
 			<RoutesNavigation />
